7dbca9499b81994c387427493194f4334b16854a
mrg <mgentry@masslight.net>
2011-07-22 10:10:04 -0400

    Switched to Cayenne 3.1 SNAPSHOT.  This needs to be used until 3.1M3 is released, due to a change in the EntitySorter interface.

--- src/main/java/org/apache/cayenne/map/CayenneEntitySorter.java
+++ src/main/java/org/apache/cayenne/map/CayenneEntitySorter.java
@@ -5,7 +5,24 @@ import java.util.List;
 
 public class CayenneEntitySorter implements EntitySorter
 {
+    /**
+     * Sets EntityResolver for this sorter. All entities present in the resolver will be
+     * used to determine sort ordering.
+     *
+     * @since 3.1
+     */
+    public void setEntityResolver(EntityResolver resolver)
+    {
 
+    }
+
+    /**
+     * Initializes a list of DataMaps used by the sorter.
+     *
+     * @deprecated since 3.1 {@link #setEntityResolver(EntityResolver)} is used, and this
+     *             method is never called.
+     */
+    @Deprecated
     public void setDataMaps(Collection<DataMap> dataMaps)
     {
         // TODO Auto-generated method stub
