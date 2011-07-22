8855a5905ff5c526d0ac6e3344bee3a0cf9b7e92
mrg <mgentry@masslight.net>
2011-07-22 09:51:04 -0400

    Added stubs for EntitySorter.

--- src/main/java/org/apache/cayenne/map/CayenneEntitySorter.java
+++ src/main/java/org/apache/cayenne/map/CayenneEntitySorter.java
@@ -1,6 +1,33 @@
 package org.apache.cayenne.map;
 
-public class CayenneEntitySorter
+import java.util.Collection;
+import java.util.List;
+
+public class CayenneEntitySorter implements EntitySorter
 {
+
+    public void setDataMaps(Collection<DataMap> dataMaps)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void sortDbEntities(List<DbEntity> dbEntities, boolean deleteOrder)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void sortObjEntities(List<ObjEntity> objEntities, boolean deleteOrder)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void sortObjectsForEntity(ObjEntity entity, List<?> objects, boolean deleteOrder)
+    {
+        // TODO Auto-generated method stub
+
+    }
 
 }
